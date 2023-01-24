6060b88219be17e4345ab6db3d1c33477a9bb012
Sandeep <wayne241101@gmail.com>
2023-01-24 19:48:40 +0530

    version 10

--- c++/C++ DSA/Pattern Problems/17.cpp
+++ c++/C++ DSA/Pattern Problems/17.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int starPattern(int n){
     for(int i = 0 ; i < n ; i++){
-        for(char ch = 'A' ; ch <= 'A' + (n - i - 1)  ; ch++){
-            cout << ch << " ";
+        char ch = 'A' + i; 
+        for(int j = 0 ; j <= i ; j++){
+            cout << " " << ch;
         }
         cout << endl;
     }
