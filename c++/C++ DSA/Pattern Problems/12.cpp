fde40d55b71139fc44df5ed260fe84d4d586c275
Sandeep <wayne241101@gmail.com>
2023-01-24 19:22:17 +0530

    version 8

--- c++/C++ DSA/Pattern Problems/12.cpp
+++ c++/C++ DSA/Pattern Problems/12.cpp
@@ -4,13 +4,30 @@ using namespace std;
 
 
 int starPattern(int n){
-    for(int i = 0 ; i < n ; i++){
-        for(int i = 0 ; i < n ; i++){
-            cout << " *";
+    int space = 2 * (n -1); // space formula
+for(int i = 1 ; i <= n ; i++)
+    {
+
+        // numbers
+        for(int j = 1; j <= i ; j++){
+            cout << j;
         }
-        cout << endl;
+
+        // space
+        for(int j = 1 ; j <= space ; j++){
+            cout << " ";
+        }
+
+        // numbers
+        for(int j = i; j >= 1 ; j--){
+            cout << j;
+        }
+        cout << endl; 
+        space -= 2;
     }
+
 }
+
 int main(){
     int n ;
     starPattern(5);
