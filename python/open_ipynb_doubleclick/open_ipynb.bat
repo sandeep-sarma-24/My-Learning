59218ad2c93175f82c77414b715ee560374ad096
Sandeep <wayne241101@gmail.com>
2023-01-30 02:40:14 +0530

    version 15

---
+++ python/open_ipynb_doubleclick/open_ipynb.bat
@@ -0,0 +1,3 @@
+@echo off
+set ANACONDAPATH=C:\ProgramData\Anaconda3
+%ANACONDAPATH%\python.exe %ANACONDAPATH%\cwp.py %ANACONDAPATH%^ %ANACONDAPATH%\python.exe %ANACONDAPATH%\Scripts\jupyter-notebook-script.py %1
